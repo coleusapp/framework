4878f6b0162ed72e47ef3b7a813e602b1920b1e0
Amirmasoud <amirmasood33@gmail.com>
2024-01-01 19:42:38 -0700

    Add s3

--- src/Coleus/Bucket/Http/Controller/BucketController.php
+++ src/Coleus/Bucket/Http/Controller/BucketController.php
@@ -3,11 +3,13 @@
 namespace Coleus\Bucket\Http\Controller;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class BucketController extends Controller
 {
     public function index()
     {
+        return Storage::disk('s3')->allFiles();
         return view('bucket::desktop');
     }
 }
