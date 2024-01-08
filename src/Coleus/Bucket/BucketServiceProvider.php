0013ddb12bb967572bd973f3a84412464f01c341
Amirmasoud <amirmasood33@gmail.com>
2024-01-08 00:15:15 -0700

    wip

--- src/Coleus/Bucket/BucketServiceProvider.php
+++ src/Coleus/Bucket/BucketServiceProvider.php
@@ -11,9 +11,9 @@ class BucketServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'bucket');
         $this->mergeConfigFrom(__DIR__.'/config/bucket.php', 'bucket');
-        $this->publishes([
-            __DIR__.'/resources/dist' => public_path('dist/coleus/bucket')
-        ], 'dist');
+        // $this->publishes([
+        //     __DIR__.'/resources/dist' => public_path('dist/coleus/bucket')
+        // ], 'dist');
 
         $this->app->bind('bucket', function ($app) {
             return new Bucket();
